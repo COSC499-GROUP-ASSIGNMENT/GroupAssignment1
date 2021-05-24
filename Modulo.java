fe0fb8302075ba4980d4710ad63bfd45ae252b58
rmeofone <46071994+rmeofone@users.noreply.github.com>
2021-05-24 13:42:08 -0700

    changed to double format and encapsulated function

--- Modulo.java
+++ Modulo.java
@@ -6,10 +6,15 @@ public class Modulo {
 	
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
-		System.out.println("Please enter the first integer");
-		int n1 = scn.nextInt();
+		System.out.println("Please enter the first number");
+		double n1 = scn.nextDouble();
 		System.out.println("Please enter the second number (modulus)");
-		int n2 = scn.nextInt();
-		System.out.println("Result of: " + n1 + " % " + n2 + " is " + n1%n2);
+		double n2 = scn.nextDouble();
+		System.out.println("Result of: " + n1 + " % " + n2 + " is " + (int) modulo(n1,n2));
+		scn.close();
+	}
+	
+	public static double modulo(double n1, double n2) {
+		return (int)n1 % (int)n2;
 	}
 }
