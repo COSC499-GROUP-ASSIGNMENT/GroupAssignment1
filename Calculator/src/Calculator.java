b03678c122af2daea9f31f9708b811050cbda384
sidnam <nambiar.siddesh@gmail.com>
2021-05-24 11:50:15 -0700

    Main branch skeleton commit.

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Calculator {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Input the first Number: ");
+		int x = sc.nextInt();
+		
+		System.out.println("Input the second number: ");
+		int y = sc.nextInt();
+		
+		//below are the output prints.
+		
+		
+		sc.close();
+		
+	}
+}
