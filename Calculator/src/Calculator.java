5b9d9b037e6fb795b583c5dc6d312a59d8afbc90
yihewang0506 <77508297+yihewang0506@users.noreply.github.com>
2021-05-24 12:14:01 -0700

    division

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -11,9 +11,21 @@ public class Calculator {
 		int y = sc.nextInt();
 		
 		//below are the output prints.
-		
+		System.out.println("The quotient of a and b is: " + division(x, y) );
+
 		
 		sc.close();
 		
+	}
+
+	public static double division(double a, double b){
+		double ans = 0;
+		if (b == 0)
+			System.out.println("You can not divide by zero.");
+		
+		else
+			ans = a/b;
+
+		return ans;
 	}
 }
